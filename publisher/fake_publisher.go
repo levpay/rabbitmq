34d9d0041a523542c8cb0c549a87aa9bb997401a
Arthur Silva <arxdsilva@gmail.com>
2020-10-17 23:21:57 -0300

    print mock publish body

--- publisher/fake_publisher.go
+++ publisher/fake_publisher.go
@@ -1,13 +1,20 @@
 package publisher
 
-import "github.com/nuveo/log"
+import (
+	"fmt"
+
+	"github.com/nuveo/log"
+)
 
 type PublisherMock struct{}
 
 func (p *PublisherMock) publishWithoutRetry(d *Declare) (err error)           { return }
 func (p *PublisherMock) PublishWithDelay(d *Declare, delay int64) (err error) { return }
-func (p *PublisherMock) Publish(d *Declare) (err error)                       { return }
 func (p *PublisherMock) createExchangeAndQueueDLX(d *Declare) (err error)     { return }
+func (p *PublisherMock) Publish(d *Declare) (err error) {
+	fmt.Printf("MOCK >>>>> %+v\n\n", string(d.Body))
+	return
+}
 
 func NewFakePublisher() (p *PublisherMock, err error) {
 	log.Println("New Publisher Mock...")
